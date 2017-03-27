7cf6ab88fef42877730f92d7169e982bac884639
Zoltan-PC\Zoltan <zoli0317@freemail.hu>
2017-03-27 22:46:56 +0200

    B

--- wp-content/themes/mentoringtheme/single/single-11.php
+++ wp-content/themes/mentoringtheme/single/single-11.php
@@ -4,6 +4,8 @@
 	<?php get_sidebar('post'); ?>	
     <?php 
 	while (have_posts()) : the_post(); ?>
+		<h2 class="title-content"><?php the_title(); ?></h2>
+		<img class="large-image" src="<?php the_post_thumbnail_url('large'); ?>"/>
         <div class="post-content"><?php the_content(); ?></div>
 		<div class="author-content">Author: <span class="author"><?php the_author(); ?></span></div>
 		<div class="date-content">Created date: <span class="date"><?php the_date(); ?></span></div>
