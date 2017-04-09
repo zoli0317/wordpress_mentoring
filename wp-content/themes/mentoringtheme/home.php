541c8a25a1746cd850237d7091494df366525d17
Zoltan-PC\Zoltan <zoli0317@freemail.hu>
2017-04-09 21:51:57 +0200

    A

--- wp-content/themes/mentoringtheme/home.php
+++ wp-content/themes/mentoringtheme/home.php
@@ -3,15 +3,21 @@
 <div class="posts">
     <?php 
 	while (have_posts()) : the_post(); ?>
-        <div class="post-content"><?php the_content(); ?></div>
-		<div class="author-content">Author: <span class="author"><?php the_author(); ?></span></div>
-		<div class="date-content">Created date: <span class="date"><?php the_date(); ?></span></div>
+        <div class="post-contents">
+        	<a class="title-contents" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        	<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+				<img class="thumbnail-image" src="<?php the_post_thumbnail_url('thumbnail'); ?>"/>
+			</a>
+			<?php the_excerpt(); ?>
+			<a href="<?php echo get_permalink(); ?>"> Read More...</a>
+			<div class="author-contents">Author: <span class="author"><?php the_author(); ?></span></div>
+			<div class="date-contents">Created: <span class="date"><?php the_date(); ?></span></div>
+        </div>
     <?php
     endwhile;
     wp_reset_query();
     ?>
 </div>
-
 <pre>This is the home.php</pre>
 
 <?php get_footer(); ?>
